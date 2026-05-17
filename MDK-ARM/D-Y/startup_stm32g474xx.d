0e6893d1246ea3d38824e930b24b98625d2a5fb4
zk <2206516139@qq.com>
2026-05-17 17:54:29 +0800

    电源板初始配置代码

---
+++ MDK-ARM/D-Y/startup_stm32g474xx.d
@@ -0,0 +1,1 @@
+d-y\startup_stm32g474xx.o: startup_stm32g474xx.s
---
+++ MDK-ARM/RTE/_D-Y/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'D-Y' 
+ * Target:  'D-Y' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
